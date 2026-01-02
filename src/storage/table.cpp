a0b52ece38c4438dcd5eda2609ac6660978a2f74
Hichemchir <hichem.C@outlook.com>
2026-01-02 10:38:22 +0100

    refactor table

--- src/storage/table.cpp
+++ src/storage/table.cpp
@@ -6,20 +6,33 @@ Table::Table(std::string table_name, std::vector<std::string> column_names)
     : name_(table_name), columns_(column_names) {}
 
 void Table::insert_row(std::vector<std::string> row) {
+    // Check if right number of col:
+    if (row.size() != columns_.size()) {
+        std::cerr << "Error: row has " << row.size()
+                  << " values but table has " << columns_.size()
+                  << " columns" << std::endl;
+        return;
+    }
+    rows_.push_back(row);
 }
 
 void Table::display() const {
-    std::cout << "Table: " << name_ << std::endl;
-    std::cout << "Cols: ";
+    std::cout << "\n Table: " << name_ << std::endl;
+    std::cout << "━━━━━━━━━━━━━━━━━━━━━━━━━━" << std::endl; 
+
     for (const auto& col : columns_) {
-        std::cout << col << "\t";
+        std::cout << std::setw(15) << col << " | ";
     }
     std::cout << std::endl;
 
-    for (const auto& line: rows_) {
-        for (const auto& value : line) {
-            std::cout << value << "\t";
+    std::cout << "━━━━━━━━━━━━━━━━━━━━━━━━━━" << std::endl;
+
+    for (const auto& row: rows_) {
+        for (const auto& value : row) {
+            std::cout << std::setw(15) << value << " | ";
         }
         std::cout << std::endl;
     }
+
+    std::cout << "\nTotal rows: " << rows_.size() << std::endl;
 }   
