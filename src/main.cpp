7caf0732d401765b81484a14259d41a47fe02cc5
Hichemchir <hichem.C@outlook.com>
2026-01-02 10:40:06 +0100

    test insert

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,14 @@
 #include "storage/table.hpp"
 
 int main() {
-    std::cout << "Minimal SQL engine" << std::endl;
-    Table users;
-    users.name = "users";
-    users.columns = {"name", "age"};
-
-    users.add_line({"Alice", "25"});
-    users.add_line({"Bob", "30"});
+    std::cout << "Minimal SQL engine\n" << std::endl;
     
+    Table users("users", {"name", "age"});
+
+    users.insert_row({"Alice", "25"});
+    users.insert_row({"Bob", "30"});
+    users.insert_row({"Charlie", "35"});
+
     users.display();
     return 0;
 }
