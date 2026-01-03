6b5bff2213a04b65ac09087e82f00f5fc522fc76
Hichemchir <hichem.C@outlook.com>
2026-01-03 18:57:57 +0100

    comment previous tests

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,27 @@
 #include <string>
 #include "storage/table.hpp"
 #include "storage/catalog.hpp"
+#include "common/types.hpp"
+#include "storage/column_schema.hpp"
 
 int main() {
-    std::cout << "Minimal SQL engine\n" << std::endl;
+    std::cout << "Minimal SQL engine with typed values\n" << std::endl;
     
+    // Test 1
     // Table users("users", {"name", "age"});
-
     // users.insert_row({"Alice", "25"});
     // users.insert_row({"Bob", "30"});
     // users.insert_row({"Charlie", "35"});
-
     // users.display();
 
+    // Test 2
+    // Catalog catalog;
+    // catalog.create_table("users", {"name", "age"});
+    // catalog.create_table("product", {"id", "product_name"});
+    // catalog.display_catalog();
+
+    // Test 3
     Catalog catalog;
-    catalog.create_table("users", {"name", "age"});
-    catalog.create_table("product", {"id", "product_name"});
-    catalog.display_catalog();
     
     return 0;
 }
