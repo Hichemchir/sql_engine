58faf292044e91644b5c05a06ba2a36f5732e91a
Hichemchir <hichem.C@outlook.com>
2026-01-03 17:00:30 +0100

    Refactor Value class: add make_integer method and improve documentation

--- include/common/types.hpp
+++ include/common/types.hpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <iostream>
 #include <variant>
+#include <cstdint>
 
 namespace sqlengine {
 
@@ -27,7 +28,25 @@ inline std::string data_type_to_string(DataType type) {
     return "UNKNOWN";
 }
 
-// use variant to rpz 1 value from many types possible. 1 at each time
+/** Use variant to rpz 1 value from many types possible. 1 at each time. Monostate --> null type,
+ *  because variant needs at least 1 valid value.
+ * Design pattern = factory methods for type safety.
+ * Static method to create object, not constructor
+ * */ 
 
+
+class Value {
+public:
+    static Value make_integer(int64_t val) {
+        Value v;
+        v.type_ = DataType::INTEGER;
+        v.data_ = val;
+        return v;
+    }
+
+private:
+    DataType type_;
+    std::variant<std::monostate, int64_t, double, std::string, bool> data_;
+};
 
 }
