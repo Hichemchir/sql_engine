2027d7778de89d44b27d7d538df8e23dab63d556
Hichemchir <hichem.C@outlook.com>
2026-01-04 09:40:21 +0100

    enhance user table creation and insertion tests, including type validation

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include "common/types.hpp"
 #include "storage/column_schema.hpp"
 
+using namespace sqlengine;
+
 int main() {
     std::cout << "Minimal SQL engine with typed values\n" << std::endl;
     
@@ -23,6 +25,45 @@ int main() {
 
     // Test 3
     Catalog catalog;
+
+    catalog.create_table("users", 
+        {
+            ColumnSchema("id", DataType::INTEGER, false, true),
+            ColumnSchema("name", DataType::TEXT, false),
+            ColumnSchema("age", DataType::INTEGER, true),
+            ColumnSchema("salary", DataType::REAL, true)
+        });
+    catalog.display_catalog();
+    std::cout << "\n━━━━━━━━━━━━━━━━━━━━━━━━━━\n" << std::endl;
+    Table* users = catalog.get_table("users");
+    if (users) {
+        users->insert_row(
+            {Value::make_integer(1),
+             Value::make_text("Hichem"),
+             Value::make_integer(27),
+             Value::make_integer(100)
+            }
+        );
+        users->insert_row({
+            Value::make_integer(2),
+            Value::make_text("Bob"),
+            Value::make_integer(30),
+            Value::make_null()  
+        });
+        users->display();
+    }
+
+    // Test if wrong type
+    bool result = users->insert_row({
+        Value::make_text("INVALID"),
+        Value::make_text("Test"),
+        Value::make_integer(99),
+        Value::make_null()
+    });
+    
+    if (!result) {
+        std::cout << "\n Type invalidation works well" << std::endl;
+    }
     
     return 0;
 }
