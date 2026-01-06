4d196830e3b35c745a6bf151917095e5204b2feb
Hichemchir <hichem.C@outlook.com>
2026-01-06 09:08:08 +0100

    structure of lexer

---
+++ include/parser/lexer.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+
+namespace sqlengine {
+
+/**
+ * Lexer:
+ *  - tokenize SQL queries (lexical analyzer)
+ *  - detects tokens (no understanding of grammar)
+ */
+
+class Lexer {
+private:
+
+
+public:
+
+
+ }
+
+} // namespace sqlengine
